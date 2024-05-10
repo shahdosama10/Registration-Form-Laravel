6e803fadee439eca445f334b2731a0263c022b11
Shahd Mostafa <mostafashahd30@gmail.com>
2024-05-11 00:25:29 +0300

    Updated test functions

--- registration_form/tests/Feature/ActorsAPITest.php
+++ registration_form/tests/Feature/ActorsAPITest.php
@@ -10,11 +10,17 @@ class ActorsAPITest extends TestCase
     public function testGetActorsWithEmptyBirthdate(){
         $response = $this->get('/getActors');
         $response->assertStatus(400)->assertJson(['error' => 'Please provide a birthdate']);
+        $response = $this->get('/getActors?birthdate=');
+        $response->assertStatus(400)
+                    ->assertJson([
+                        'error' => 'Please provide a birthdate'
+                    ]);
+    
     } 
 
 
     public function testFetchActorsWithInvalidBirthdateFormat(){
-        $response = $this->get('/getActors?birthdate=');
+        $response = $this->get('/getActors?month=15&day=35');
         $response->assertStatus(400)
                     ->assertJson([
                         'error' => 'Invalid birthdate format'
@@ -27,7 +33,7 @@ class ActorsAPITest extends TestCase
             '*' => Http::response([], 500)
         ]);
 
-        $response = $this->get('/getActors?birthdate=05-10');
+        $response = $this->get('/getActors?month=10&day=10');
         $response->assertStatus(500)
                  ->assertJson([
                      'error' => 'Failed to fetch data from the API. Please try again later.'
